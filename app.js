179dd04545502d405724d49bd14e303bdc9a5c70
Tom Clemente <tomangelo.clm@gmail.com>
2021-04-27 05:33:54 +0800

    added photo places api and bug fixes

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ var browser = null;
 const BOOK_API = "https://www.googleapis.com/books/v1/volumes/";
 const YOUTUBE_API = "https://www.googleapis.com/youtube/v3/videos?id=";
 const PLACE_API = "https://maps.googleapis.com/maps/api/place/details/json?place_id=";
+const PLACE_PHOTO__API = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=";
 
 exports.handler = async (event, context) => {
 
@@ -36,11 +37,11 @@ exports.handler = async (event, context) => {
     const promises = [];
     const promisesAPI = [];
     let statusCode = '200';
-    
+
     // enables CORS
     const headers = {
         'Content-Type': 'application/json',
-        "Access-Control-Allow-Headers" : "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "OPTIONS,POST"
     };
@@ -48,15 +49,16 @@ exports.handler = async (event, context) => {
     try {
 
         body = await new Promise((resolve, reject) => {
-      
+
             switch (event.httpMethod) {
                 case 'POST':
                     apilist = new Array();
                     resp = new Object();
+                    browser = null;
 
-                    for (var i = 0; i < params.length; i++) {                    
+                    for (var i = 0; i < params.length; i++) {
                         id = params[i].id;
-                        type = params[i].type;                            
+                        type = params[i].type;
                         filename = type + "_" + id;
 
                         if (type == 'url') {
@@ -67,55 +69,55 @@ exports.handler = async (event, context) => {
                         // check all requests if it exists in S3
                         promises.push(getFile(filename, type, id));
                     }
-                break;
+                    break;
 
                 default:
                     throw new Error(`Unsupported method "${event.httpMethod}"`);
             }
 
-            Promise.all(promises).then(function() {
+            Promise.all(promises).then(function () {
                 console.log("final callAPI: ", apilist);
 
                 for (var i = 0; i < apilist.length; i++) {
-                    switch (apilist[i].type) {    
+                    switch (apilist[i].type) {
                         case 'place':
-                            promisesAPI.push(callAPI(PLACE_API, 
+                            promisesAPI.push(callAPI(PLACE_API,
                                 apilist[i].type, apilist[i].id));
-                        break;
+                            break;
 
                         case 'youtube':
-                            promisesAPI.push(callAPI(YOUTUBE_API, 
+                            promisesAPI.push(callAPI(YOUTUBE_API,
                                 apilist[i].type, apilist[i].id));
-                        break;
-                        
-                        case 'book':                                
-                            promisesAPI.push(callAPI(BOOK_API, 
+                            break;
+
+                        case 'book':
+                            promisesAPI.push(callAPI(BOOK_API,
                                 apilist[i].type, apilist[i].id));
-                        break;
+                            break;
 
                         case 'url':
                             promisesAPI.push(takeScreenshot(apilist[i].id));
-                        break;
+                            break;
 
                         default:
                             throw new Error(`Unsupported type "${apilist[i].type}"`);
                     }
-                   
+
                 }
-     
-                Promise.all(promisesAPI).then(function() {
+
+                Promise.all(promisesAPI).then(function () {
                     console.log("resp: ", resp);
                     resolve(resp);
                 });
-            });    
+            });
         });
-        
+
 
     } catch (err) {
         statusCode = '400';
         body = err;
         console.log("body return error: ", err);
-        
+
     } finally {
         body = JSON.stringify(body);
     }
@@ -124,48 +126,125 @@ exports.handler = async (event, context) => {
         statusCode,
         body,
         headers,
-    };S
+    }; S
 }
 
-async function callAPI(url, type, id) {    
+async function callAPI(url, type, id) {
     let dataString = '';
+    let result;
 
-    var link = url + id;
-    if (type == 'place' || type == "youtube") {
+    let link = url + id;
+    let name = type + "_" + id;
+
+    if (type == 'place') {
         link += "&key=" + API_KEY;
+
+    } else if (type == 'youtube') {
+        link += "&key=" + API_KEY + "&part=snippet";
     }
 
     console.log("API link: ", link);
-    
+
     const response = await new Promise((resolve, reject) => {
-        const req = https.get(link , function(res) {
+        const req = https.get(link, function (res) {
 
-          res.on('data', chunk => {
-            dataString += chunk;
-          });
+            res.on('data', chunk => {
+                dataString += chunk;
+            });
 
-          res.on('end', () => {
-
-            let name = type + "_" + id;
-            resp[name] = JSON.parse(dataString);
-
-            copyToS3(dataString, name).then(function() {
-                resolve({
-                    statusCode: 200,
-                    body: JSON.parse(dataString)
-                });
-            }, reject);
-
-          });
+            res.on('end', () => {
+                result = JSON.parse(dataString);
+                resolve();
+            });
         });
-        
+
         req.on('error', (e) => {
-          reject({
-              statusCode: 500,
-              body: 'Something went wrong!'
-          });
+            reject({
+                statusCode: 500,
+                body: 'Something went wrong!'
+            });
         });
     });
+
+    if (type == 'youtube') {
+        result['items'] = result.items[0];
+
+    } else if (type == 'place') {
+        result = result['result'];
+
+        for (var x = 0; x < result.photos.length; x++) {
+            let resp = await callPlacePhotosAPI(result.photos[x].photo_reference);
+            result.photos[x]["url"] = resp.body;
+        }
+    }
+
+    console.log("final result: ", result);
+
+    resp[name] = result;
+
+    await copyToS3(result, name);
+
+    return response;
+}
+
+async function callPlacePhotosAPI(photoReference) {
+    let buffer;
+    let link = PLACE_PHOTO__API + photoReference + "&key=" + API_KEY;
+
+    console.log("Place Photos Link: ", link);
+
+    let response = await new Promise((resolve, reject) => {
+        const req = https.get(link, function (res) {
+
+            res.on('data', chunk => {
+                buffer += chunk;
+            });
+
+            res.on('end', () => {
+                var url = buffer.substring(
+                    buffer.lastIndexOf("HREF=") + 6,
+                    buffer.lastIndexOf("here") - 2
+                );
+
+                console.log("url :", url);
+                resolve({
+                    statusCode: 200,
+                    body: url
+                });
+            });
+        });
+
+        req.on('error', (e) => {
+            reject({
+                statusCode: 500,
+                body: 'Something went wrong!'
+            });
+        });
+    });
+
+    // const params = {
+    //     Bucket: dstBucket,
+    //     Key: "place_" + photoReference + ".png",
+    //     Body: buffer,
+    //     ContentType: 'image/png',
+    //     ACL: 'public-read'
+    // };
+
+    // const uploadResponse = await new Promise((resolve, reject) => {
+    //     s3client.upload(params, function (s3Err, data) {
+    //         if (s3Err) {
+    //             console.log("File uploaded s3Err at: ", s3Err);
+    //             reject(s3Err);
+    //         } else {
+    //             //resp["url_" + filename] = data.Location;
+    //             console.log(`File uploaded successfully at ${data.Location}`);
+    //             resolve({
+    //                 statusCode: 200,
+    //                 location: data.Location
+    //             });
+    //         }
+    //     });
+    // });
 
     return response;
 }
@@ -174,18 +253,18 @@ async function copyToS3(content, filename) {
 
     const params = {
         Bucket: dstBucket,
-        Key: filename + ".json", 
+        Key: filename + ".json",
         Body: JSON.stringify(content, null, 2)
     };
 
     const response = await new Promise((resolve, reject) => {
-        s3client.upload(params, function(s3Err, data) {
+        s3client.upload(params, function (s3Err, data) {
             if (s3Err) {
                 reject(s3Err);
             } else {
                 console.log(`File uploaded successfully at ${data.Location}`);
                 resolve();
-            }            
+            }
         });
     });
 
@@ -193,25 +272,25 @@ async function copyToS3(content, filename) {
 }
 
 async function getFile(filename, type, id) {
-    
+
     let key = filename + ".json";
     if (type == 'url') {
-        key =  filename + ".png";
+        key = filename + ".png";
     }
 
     const params = {
         Bucket: dstBucket,
-        Key: key, 
+        Key: key,
     };
 
     console.log("Retrieving file in S3: ", key);
 
     const response = await new Promise((resolve, reject) => {
-        s3client.getObject(params, function(err, result) {
+        s3client.getObject(params, function (err, result) {
             if (err) {
                 console.log(err, err.stack);
-                apilist.push({type: type, id: id});
-                resolve({statusCode: 404, data: "Object not found.", err: err.stack});
+                apilist.push({ type: type, id: id });
+                resolve({ statusCode: 404, data: "Object not found.", err: err.stack });
 
             } else {
                 let objectData = result.Body.toString('utf-8');
@@ -219,12 +298,12 @@ async function getFile(filename, type, id) {
                 if (type == 'url') {
                     resp["url_" + filename] = "https://" + dstBucket + ".s3.amazonaws.com/" + encodeURIComponent(key);
                 } else {
-                    resp[filename] = JSON.parse(JSON.parse(objectData));
+                    resp[filename] = JSON.parse(objectData);
                 }
-                
-                resolve({statusCode: 200, data: objectData, type: type, id: id});              
+
+                resolve({ statusCode: 200, data: objectData, type: type, id: id });
             }
-        });    
+        });
     });
 
     return response;
@@ -243,7 +322,7 @@ async function prepBrowser() {
 async function takeScreenshot(id) {
     let filename = id;
     let response;
-    
+
     try {
         if (browser == null) {
             browser = await chromium.puppeteer.launch({
@@ -288,14 +367,14 @@ async function takeScreenshot(id) {
         });
 
         await page.close();
-   
+
     } catch (error) {
         console.log(error);
 
     } finally {
-        // if (browser !== null) {
-        //     await browser.close();
-        // }
+        if (browser !== null) {
+            await browser.close();
+        }
     }
 
     return response;
